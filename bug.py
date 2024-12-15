f36dd14d9564e85ec1eb711ba66314eb5056f9c1
BugHunter-X <askzenai@gmail.com>
2024-12-15 01:54:05 +0100

    Add bug.py file

---
+++ bug.py
@@ -0,0 +1,13 @@
+def my_function(a, b):
+    if a > b:
+        return a
+    return b
+
+result = my_function(5, 2)
+print(result)  # Output: 5
+
+result = my_function(2, 5)
+print(result)  # Output: 5
+
+result = my_function(5, 5)
+print(result) # Output: 5
